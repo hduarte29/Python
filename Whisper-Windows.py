8442129e7f990af393f917f153dedeeba32e73ba
herduarte29 <herduarte.29@gmail.com>
2023-05-26 18:43:08 -0300

    algo

--- Whisper-Windows.py
+++ Whisper-Windows.py
@@ -4,6 +4,7 @@ model = whisper.load_model("large")
 result = model.transcribe("C:\\Users\\Hernan\\OneDrive\\Hduarte\\01 - Estudios\\Intercambio\\3 - Práctica Profesionalizante III Entrevista\\Entrevistas\\Entrevista 2\\Entrevista 2.mp4")
 archi = open ("C:\\Users\\Hernan\\OneDrive\\Hduarte\\01 - Estudios\\Intercambio\\3 - Práctica Profesionalizante III Entrevista\\Entrevistas\\Entrevista 2\\Entrevista 2.txt","w")
 archi.write(result["text"])  
-archi.close() 
+archi.close()
+
 print(result["text"])
 
